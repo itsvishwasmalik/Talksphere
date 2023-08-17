6335817c510606d69f866453a260bafc9248c69a
itsvishwasmalik <vishwasmalik2003@gmail.com>
2023-08-17 22:03:59 +0530

    room

--- frontend/src/pages/Components/Room.js
+++ frontend/src/pages/Components/Room.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, InputAdornment, Typography } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import {
-    getAvatarColors,
-    getTimeDifference,
-} from "../../utils/getTimeDifference";
+import Avatar from "@mui/material/Avatar";
+import { Link } from "react-router-dom";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import SendIcon from "@mui/icons-material/Send";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { getTimeDifference, getAvatarColors } from "../../utils";
+import { useTheme } from "@emotion/react";
 
-const Room = ({ room, room_messages, paricipants }) => {
+const Room = ({ activity, room, room_messages, paricipants }) => {
+    const theme = useTheme();
     return (
         <Box>
-            {/* Chat Component */}
+            {/* Chat Description */}
             <Box>
                 <Box>
                     <ArrowBackIosIcon />
@@ -50,11 +55,86 @@ const Room = ({ room, room_messages, paricipants }) => {
                             </Typography>
                         </Box>
                     </Box>
+                    <Typography>{room.description}</Typography>
+                    <Box
+                        sx={{
+                            backgroundColor: theme.palette.background.paper,
+                            borderRadius: "10px",
+                            padding: "5px 15px 5px 15px",
+                            paddingRight: "15px",
+                        }}
+                    >
+                        <Typography sx={{ fontSize: "12px" }}>
+                            {room.topic}
+                        </Typography>
+                    </Box>
+                </Box>
+
+                {/* Message Component */}
+                <Box>
+                    <Box>
+                        <Box>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    flexDirection: "row",
+                                }}
+                            >
+                                <Box>
+                                    <Avatar
+                                        sx={{
+                                            backgroundColor: getAvatarColors(
+                                                activity.name
+                                            ),
+                                        }}
+                                    >
+                                        {activity.name[0].toUpperCase()}
+                                    </Avatar>
+                                </Box>
+                                <Box>
+                                    <Box sx={{ marginLeft: "10px" }}>
+                                        <Typography
+                                            component={Link}
+                                            sx={{
+                                                textDecoration: "none",
+                                                color: theme.palette.secondary
+                                                    .main,
+                                            }}
+                                        >
+                                            @{activity.name}
+                                        </Typography>
+                                    </Box>
+                                    <Box>
+                                        <Typography
+                                            sx={{ fontSize: "12px", pl: 2 }}
+                                        >
+                                            {getTimeDifference(
+                                                activity.created
+                                            )}{" "}
+                                            ago
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </Box>
+                            <DeleteIcon />
+                        </Box>
+                        <Box></Box>
+                    </Box>
+
+                    <FormControl fullWidth sx={{ m: 1 }}>
+                        <OutlinedInput
+                            id="outlined-adornment-amount"
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    $
+                                </InputAdornment>
+                            }
+                            label="Amount"
+                        />
+                        <SendIcon />
+                    </FormControl>
                 </Box>
             </Box>
-
-            {/* Participants Component */}
-            <Box></Box>
         </Box>
     );
 };
